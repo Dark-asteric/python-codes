9199ff08d22973686db911f92e0277e3555c0189
Muhammad MI Shafi <111156790+Dark-asteric@users.noreply.github.com>
2023-10-03 12:22:17 +0200

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,15 @@
+﻿import random
+import string
+import secrets
+
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+
+alphabet = letters + digits + special_chars
+
+n = int(input())
+password = ""
+for i in range(n):
+	password += ''.join(secrets.choice(alphabet))
+print(f"Your password is here : {password}")
